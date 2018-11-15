de8582d2dd41072e4934864e69e1dbce6a3d59d0
Alaina Johnson <alainakjohnson@gmail.com>
2018-11-15 12:19:30 -0500

    NavBar implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import NavBar from './components/layout/Navbar.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar />
         <h1>My React App</h1>
       </div>
     );
