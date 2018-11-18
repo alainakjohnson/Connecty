c18aea5bf9dd09ad285951f8fcf76d34b92535c9
Alaina Johnson <alainakjohnson@gmail.com>
2018-11-18 15:37:48 -0500

    successfully registered a user

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,61 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Register extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            email: "",
+            password: "",
+            password2: "",
+            error: {} // will be explained later
+        };
+
+        this.onInputChange=this.onInputChange.bind(this);
+        this.onFormSubmit=this.onFormSubmit.bind(this);
+        
+    //     this.state = { term: '' };
+    //     this.onInputChange=this.onInputChange.bind(this);
+    //     this.onFormSubmit=this.onFormSubmit.bind(this);
+    //     //note: binds the 'this' reference inside the callback function to 
+    //     //the SearchBar object so when the callback is called, 'this' has the 
+    //     //correct context. can also use onInputChange = (event) =>{}
+    }
+    
+    // onInputChange(event){
+    //     console.log(event.target.value);
+    //     this.setState({term: event.target.value});
+    // }
+
+    onInputChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+    
+    // onFormSubmit(event){
+    //     event.preventDefault();
+    //     //note: prevents default behavior of the HTML form element
+    //     this.props.fetchWeather(this.state.term);
+    //     this.setState({ term: ' ' });
+    //}
+
+    onFormSubmit(event) {
+        event.preventDefault();
+        this.setState({ term: ' ' });
+    
+        const newUser = {
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password,
+            password2: this.state.password2
+        };
+    
+        axios
+            .post("api/users/register", newUser)
+            .then(res => console.log(res.data))
+            .catch(err => console.log(err.response.data));
+    }
 
     render() {
         return (
@@ -11,19 +66,47 @@ class Register extends Component {
                             <div className="col-md-8 m-auto">
                             <h1 className="display-4 text-center">Sign Up</h1>
                             <p className="lead text-center">Create your Connecty account</p>
-                            <form action="create-profile.html">
+                            <form 
+                                noValidate
+                                onSubmit={this.onFormSubmit}
+                                action="create-profile.html">
+
                                 <div className="form-group">
-                                <input type="text" className="form-control form-control-lg" placeholder="Name" name="name" required />
+                                <input type="text" 
+                                className="form-control form-control-lg" 
+                                placeholder="Name" 
+                                name="name" required 
+                                value={this.state.name}
+                                onChange={this.onInputChange}
+                                />
                                 </div>
                                 <div className="form-group">
-                                <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" />
+                                <input type="email" 
+                                className="form-control form-control-lg" 
+                                placeholder="Email Address" 
+                                name="email" 
+                                value={this.state.email}
+                                onChange={this.onInputChange}
+                                />
                                 <small classNameName="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
                                 </div>
                                 <div className="form-group">
-                                <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" />
+                                <input type="password" 
+                                className="form-control form-control-lg"
+                                placeholder="Password" 
+                                name="password"
+                                value={this.state.password}
+                                onChange={this.onInputChange}
+                                />
                                 </div>
                                 <div className="form-group">
-                                <input type="password" className="form-control form-control-lg" placeholder="Confirm Password" name="password2" />
+                                <input type="password" 
+                                className="form-control form-control-lg" 
+                                placeholder="Confirm Password" 
+                                name="password2" 
+                                value={this.state.password2}
+                                onChange={this.onInputChange}
+                                />
                                 </div>
                                 <input type="submit" className="btn btn-info btn-block mt-4" />
                             </form>
