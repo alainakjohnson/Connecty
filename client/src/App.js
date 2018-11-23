52be4e0c6d9de32d7a23d9f79cc9c54a05ede2c9
Alaina Johnson <alainakjohnson@gmail.com>
2018-11-23 15:37:45 -0500

    Implemented Reducers

--- client/src/App.js
+++ client/src/App.js
@@ -10,11 +10,13 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
+const initialState = {};
 const middleware = [thunk];
 const store = createStore(
-    () => [], // empty store since no reducer created yet
-    {},
+    rootReducer,
+    initialState,
     composeWithDevTools(applyMiddleware(...middleware))
 );
 
